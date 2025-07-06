506a6f8a02c03bcc1ce0d0e39f9118083e3750dd
Kabwaga Veronica Amooti <kabwagavera@gmail.com>
2025-07-06 21:48:55 +0300

    I made changes to files, test.js, multiply.js , my first tests failed since I had not defined the assertEqual function, it worked only doing it right, I also tried to use different digits of multiplying 2*3 returning 4 and it still passed as my condition

--- test/test.js
+++ test/test.js
@@ -1,8 +1,17 @@
-const { assert } = require('assert');
-const multiply = require('../multiply');
+// const assertEqual = require('assert');
+// const {multiply} = require('../multiply');
+// 
+// describe('multiply()', () => {
+//   it('should return 1 when multiplying 1 * 1', () => {
+//     assertEqual(multiply(1, 1), 1);
+//   });
+// });
 
-describe('multiply()', function () {
-  it('should return 1 when multiplying 1 * 1', function () {
-    assert.equal(multiply(1, 1), 1);
-  });
+const assertEqual = require('assert');
+const { multiply } = require('../multiply');
+describe('multiply',()=> {
+    it('should return 4 when multipling 2 * 2' , () => {
+      assertEqual(multiply(2, 2) ,4) ;
+    });
 });
+
