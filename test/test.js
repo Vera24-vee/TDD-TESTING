657d3698692bc9a3183c35a6175a8c9cb56a0042
Kabwaga Veronica Amooti <kabwagavera@gmail.com>
2025-07-06 22:01:40 +0300

    my test has failed because of a type error, multiply is not a function

--- test/test.js
+++ test/test.js
@@ -7,11 +7,30 @@
 //   });
 // });
 
+// const assertEqual = require('assert');
+// const { multiply } = require('../multiply');
+// describe('multiply',()=> {
+//     it('should return 4 when multipling 2 * 2' , () => {
+//       assertEqual(multiply(2, 2) ,4) ;
+//     });
+// });
+
+
+// const assertEqual = require('assert');
+// const { multiply } = require('../multiply');
+// describe('multiply',()=> {
+//     it('should return 9 when multipling 3 * 3' , () => {
+//      assertEqual(multiply(3, 3), 9) ;
+//     });
+// });
+
 const assertEqual = require('assert');
 const { multiply } = require('../multiply');
 describe('multiply',()=> {
-    it('should return 4 when multipling 2 * 2' , () => {
-      assertEqual(multiply(2, 2) ,4) ;
+    it('should return 16 when multipling 4 * 4' , () => {
+     assertEqual(multiply(4 , 4), 16) ;
     });
 });
+
+
 
