9fb7f458edef40dcc69ebbcc4ca9527ca692c861
Kabwaga Veronica Amooti <kabwagavera@gmail.com>
2025-07-07 21:57:19 +0300

    my test for 4! was also successful

--- test/factorial.test.js
+++ test/factorial.test.js
@@ -11,15 +11,15 @@ describe('factorial()', () => {
 //   it('should return 1 for 1!', () => {
 //     assert.strictEqual(factorial(1), 1);
 //   });
+// 
+//   it('should return 6 for 3!', () => {
+//     assert.strictEqual(factorial(3), 6);
+//   });
 
-  it('should return 6 for 3!', () => {
-    assert.strictEqual(factorial(3), 6);
+  it('should return 24 for 4!', () => {
+    assert.strictEqual(factorial(4), 24);
   });
 
-//   it('should return 24 for 4!', () => {
-//     assert.strictEqual(factorial(4), 24);
-//   });
-// 
 //   it('should return 120 for 5!', () => {
 //     assert.strictEqual(factorial(5), 120);
 //   });
