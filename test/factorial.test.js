14f6023ec51f057e8babd96f7eda6ee5208a2352
Kabwaga Veronica Amooti <kabwagavera@gmail.com>
2025-07-07 22:00:15 +0300

    my test for 6! was also successful

--- test/factorial.test.js
+++ test/factorial.test.js
@@ -16,18 +16,18 @@ describe('factorial()', () => {
 //     assert.strictEqual(factorial(3), 6);
 //   });
 
-  it('should return 24 for 4!', () => {
-    assert.strictEqual(factorial(4), 24);
-  });
+//   it('should return 24 for 4!', () => {
+//     assert.strictEqual(factorial(4), 24);
+//   });
 
 //   it('should return 120 for 5!', () => {
 //     assert.strictEqual(factorial(5), 120);
 //   });
-// 
-//   it('should return 3628800 for 10!', () => {
-//     assert.strictEqual(factorial(10), 3628800);
-//   });
-// 
+
+  it('should return 3628800 for 10!', () => {
+    assert.strictEqual(factorial(10), 3628800);
+  });
+
 //   it('should throw an error for negative numbers', () => {
 //     assert.throws(() => factorial(-5), Error, "Negative numbers not allowed");
 //   });
