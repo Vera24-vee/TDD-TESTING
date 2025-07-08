bca1ae7ccbe853dba921e6369e5c0dcb76db3894
Kabwaga Veronica Amooti <kabwagavera@gmail.com>
2025-07-08 23:36:54 +0300

    my first test failed because of a type error, fibonacci is not a function

--- factorial.js
+++ factorial.js
@@ -1,9 +1,9 @@
-// TDD Test Assignment 1, Pair: Angwen Sarah and Kabwaga Veronica Amooti
-
-function factorial(n) {
-  if (n < 0) throw new Error("Negative numbers not allowed");
-  if (n === 0) return 1;
-  return n * factorial(n - 1);
-}
-
-module.exports = { factorial };
+// // TDD Test Assignment 1, Pair: Angwen Sarah and Kabwaga Veronica Amooti
+// 
+// function factorial(n) {
+//   if (n < 0) throw new Error("Negative numbers not allowed");
+//   if (n === 0) return 1;
+//   return n * factorial(n - 1);
+// }
+// 
+// module.exports = { factorial };
